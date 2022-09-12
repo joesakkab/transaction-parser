d0bf8433092c7d93b4ffe7ab9b45275bf889ed28
Joe Sakkab <Joe.Sakkab@progressoft.io>
2022-09-12 11:04:48 +0300

    Comments fix and gitignore file

--- src/main/java/CSVTransactionParser.java
+++ src/main/java/CSVTransactionParser.java
@@ -10,27 +10,35 @@ public class CSVTransactionParser implements TransactionParser{
     // Implementation of TransactionParser interface.
     @Override
     public List<Transaction> parse(File transactionsFile) {
-        Scanner sc = checkFileformat(transactionsFile);
-        List<Transaction> list = new ArrayList<Transaction>();
-        String line = "";
-        while (sc.hasNextLine()) {
-            line = sc.nextLine();
-            String[] tokens = line.split(",");
-            list.add(checkErrorsAndConvert(tokens));
+        validateFileFormat(transactionsFile);
+        try {
+            Scanner sc = new Scanner(transactionsFile);
+            List<Transaction> list = new ArrayList<>();
+            String line;
+            while (sc.hasNextLine()) {
+                line = sc.nextLine();
+                String[] tokens = line.split(",");
+                list.add(checkErrorsAndConvert(tokens));
+            }
+            return list;
+        } catch (FileNotFoundException e) {
+            throw new TransactionParserException("Please make sure file is valid");
         }
-        return list;
     }
 
-    private Scanner checkFileformat(File transactionsFile) {
+    private Transaction checkErrorsAndConvert(String[] tokens) {
+        checkTransactionFields(tokens);
+        checkIfNumeric(tokens[2]);
+        Transaction t = new Transaction();
+        t.setDescription(tokens[0]);
+        t.setDirection(tokens[1]);
+        t.setAmount(checkIfNumeric(tokens[2]));
+        t.setCurrency(tokens[3]);
+        return t;
+    }
+    private void validateFileFormat(File transactionsFile) {
         if (!transactionsFile.getName().endsWith(".csv")) {
             throw new TransactionParserException("Please make sure file is in csv format (ends with .csv)");
-        }
-
-        try {
-            Scanner scan = new Scanner(transactionsFile);
-            return scan;
-        } catch (Exception e) {
-            throw new TransactionParserException("", e);
         }
     }
     private void checkTransactionFields(String[] tokens) {
@@ -47,21 +55,10 @@ public class CSVTransactionParser implements TransactionParser{
 
     private BigDecimal checkIfNumeric(String token) {
         try {
-            BigDecimal bd = new BigDecimal(token);
-            return bd;
+            return new BigDecimal(token);
         } catch (Exception e){
             throw new TransactionParserException("Please make sure transaction amount is numeric");
         }
-    }
-    private Transaction checkErrorsAndConvert(String[] tokens) {
-        checkTransactionFields(tokens);
-        checkIfNumeric(tokens[2]);
-        Transaction t = new Transaction();
-        t.setDescription(tokens[0]);
-        t.setDirection(tokens[1]);
-        t.setAmount(checkIfNumeric(tokens[2]));
-        t.setCurrency(tokens[3]);
-        return t;
     }
 
 
--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,17 +1,16 @@
 import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.List;
 
 //TODO Convert to DTO
 public class Transaction {
     private String description;
-    private enum direction {
+    private Direction direction;
+    private BigDecimal amount;
+    private Currency currency;
+    private enum Direction {
         CREDIT,
         DEBIT
     }
-    private direction dir;
-    private BigDecimal amount;
-    private Currency cur;
     public String getDescription(){
         return this.description;
     }
@@ -20,15 +19,15 @@ public class Transaction {
         this.description = givenDescription;
     }
 
-    public direction getDirection() {
-        return this.dir;
+    public Direction getDirection() {
+        return this.direction;
     }
 
     public void setDirection(String givenDirection){
         if (givenDirection.equals("Credit")) {
-            this.dir = direction.CREDIT;
+            this.direction = Direction.CREDIT;
         } else if (givenDirection.equals("Debit")) {
-            this.dir = direction.DEBIT;
+            this.direction = Direction.DEBIT;
         }
     }
 
@@ -41,16 +40,16 @@ public class Transaction {
     }
 
     public String getCurrency(){
-        return this.cur.getCurrencyCode();
+        return this.currency.getCurrencyCode();
     }
 
     public void setCurrency(String givenCurrency){
-        this.cur = Currency.getInstance(givenCurrency);
+        this.currency = Currency.getInstance(givenCurrency);
     }
 
     @Override
     public String toString() {
-        String result = "";
+        String result;
         result = "Description: " + this.getDescription() + "\n   " +
                 "Direction: " + this.getDirection() + "\n   " +
                 "Amount: " + this.getAmount() + "\n   " +
